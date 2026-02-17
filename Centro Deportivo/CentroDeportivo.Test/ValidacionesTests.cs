2ad55ba5b485efde5268629d38f4ef5c281f027c
María Emilia Navalón Vidal <marnavvid2@alu.edu.gva.es>
2026-02-17 20:49:46 +0100

    Update ValidacionesTests.cs
    Removed commented sections for Arrange, Act, and Assert in tests.

--- Centro Deportivo/CentroDeportivo.Test/ValidacionesTests.cs
+++ Centro Deportivo/CentroDeportivo.Test/ValidacionesTests.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ViewModel.Services; // Donde está tu SocioService
-using Model;             // Donde están tus clases de datos
+using ViewModel.Services; 
+using Model;             
 
 namespace CentroDeportivo.Tests
 {
@@ -10,28 +10,27 @@ namespace CentroDeportivo.Tests
         [TestMethod]
         public void ValidarEmail_FormatoCorrecto_RetornaTrue()
         {
-            // 1. Arrange (Preparar)
+
             var service = new SocioService();
             string emailValido = "alumno@estacio.com";
 
-            // 2. Act (Ejecutar)
+
             bool resultado = service.ValidarEmailFormato(emailValido);
 
-            // 3. Assert (Verificar)
             Assert.IsTrue(resultado, "El email debería ser válido");
         }
 
         [TestMethod]
         public void ValidarEmail_SinArroba_RetornaFalse()
         {
-            // Arrange
+
             var service = new SocioService();
             string emailInvalido = "alumno.com";
 
-            // Act
+
             bool resultado = service.ValidarEmailFormato(emailInvalido);
 
-            // Assert
+      
             Assert.IsFalse(resultado, "El email no debería ser válido sin el @");
         }
     }
